704ab103ef6486a56d3c84ae18104b4929da4ff6
oluwatomilayoshittu <oshittu@tfs.ca>
2021-02-26 09:38:05 -0500

    2

--- app/src/main/java/com/tomi/pokeview/MainActivity.java
+++ app/src/main/java/com/tomi/pokeview/MainActivity.java
@@ -6,6 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
     private List<ListItem> listItems;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +37,50 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        String thetype;
+
         listItems=new ArrayList<>();
         for(int i=0; i<10; i++)
         {
-            ListItem item=new ListItem("Article "+(i+1), "Description");
+            ListItem item=new ListItem("Pokemon "+(i+1), "PokeType...");
             listItems.add(item);
+
+            String eye = String.valueOf(i);
+            String url = "https://pokeapi.co/api/v2/pokemon/"+ eye;
+
+            JsonObjectRequest jsonObjectRequest=new JsonObjectRequest("https://pokeapi.co/api/v2/pokemon/1", Request.Method.GET, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response)
+                {
+                    JSONArray tabTypes = null;
+                    try {
+                        tabTypes = response.getJSONArray("types");
+
+                        // pokemon type
+                        JSONObject jsonobj = (JSONObject) tabTypes.get(0);
+                        JSONObject Typexx = (JSONObject) jsonobj.get("type");
+                        String typeName = Typexx.getString("name");
+                       // ab1.setText(typeName);                                // THIS LINE DOESNT WORK BUT THE "REQUEST METHOD GET" WONT WORK WITHOUT THIS LINE.
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+
         }
         adapter=new MyAdapter(this, listItems);
         recyclerView.setAdapter(adapter);
+
+
     }
+
+
+
 }
